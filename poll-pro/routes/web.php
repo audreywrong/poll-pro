e6a856a484cd5c6918d60831f860c25cbd3b5b70
Audrey <audreywrong@gmail.com>
2021-03-26 17:35:55 -0500

    testing routes
    test routes working with controllers

--- poll-pro/routes/web.php
+++ poll-pro/routes/web.php
@@ -20,3 +20,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/polls/create', [App\Http\Controllers\PollController::class, 'create'])->name('poll.create');
+
+// Route::get('/admin/polls/create', [App\Http\Controllers\HomeController::class, 'index'])->name('poll.create');
+
+// Route::get('/admin/polls/create', ['uses' => 'PollManagerController@create', 'as' => 'poll.create']);
+
+// $prefix = config('larapoll_config.prefix');
+// Route::group(['namespace' => 'Inani\Larapoll\Http\Controllers', 'prefix' => $prefix, 'middleware' => 'web'], function () {
+//     Route::get('/admin/polls/create', [App\Http\Controllers\PollManagerController::class, 'create'])->name('poll.create');
+//     // $middleware = config('larapoll_config.admin_auth');
+
+//     // $guard = config('larapoll_config.admin_guard');
+//     // Route::middleware(["$middleware:$guard"])->group(function () {
+
+//     // });
+
+//     // Route::post('/vote/polls/{poll}', 'VoteManagerController@vote')->name('poll.vote');
+// });
