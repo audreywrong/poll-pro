38aff1805d8ddb640067e8505e654839622e70ae
Audrey <audreywrong@gmail.com>
2021-03-26 18:53:13 -0500

    poll table adjustments

--- poll-pro/resources/js/components/PollTable.js
+++ poll-pro/resources/js/components/PollTable.js
@@ -5,24 +5,13 @@ import { DataGrid } from "@material-ui/data-grid";
 
 const columns = [
     { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "firstName", headerName: "Question", width: 300 },
+    { field: "lastName", headerName: "Created", width: 130 },
     {
         field: "age",
-        headerName: "Age",
+        headerName: "Number of Options",
         type: "number",
-        width: 90,
-    },
-    {
-        field: "fullName",
-        headerName: "Full name",
-        description: "This column has a value getter and is not sortable.",
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.getValue("firstName") || ""} ${
-                params.getValue("lastName") || ""
-            }`,
+        width: 200,
     },
 ];
 
