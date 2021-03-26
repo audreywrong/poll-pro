b4dcdb75b8c4243c6265e2a5e5e2299bc952479b
Audrey <audreywrong@gmail.com>
2021-03-26 17:36:15 -0500

    cleaning code

--- poll-pro/resources/views/layouts/app.blade.php
+++ poll-pro/resources/views/layouts/app.blade.php
@@ -80,21 +80,10 @@
                         </li>
                         @endguest
                     </ul>
-                    <div class="block lg:hidden">
-                        <button
-                            class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <title>Menu</title>
-                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="flex w-full lg:flex lg:items-center lg:w-auto">
-                        <div>
-                            <a href="{{ route('poll.create') }}"
-                                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Create
-                                new Poll</a>
-                        </div>
+                    <div>
+                        <a href="{{ route('poll.create') }}">
+                            Create new Poll
+                        </a>
                     </div>
                 </div>
             </div>
