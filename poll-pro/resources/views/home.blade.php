447bbaf8ee2643340d6919388b9dc3482029a93d
Audrey <audreywrong@gmail.com>
2021-03-26 22:02:31 -0500

    getting poll table data
    lots of compiling

--- poll-pro/resources/views/home.blade.php
+++ poll-pro/resources/views/home.blade.php
@@ -7,7 +7,6 @@
     @auth
     <script>
         var GUSER = {{ Auth::user()->id }};
-        // var GPOLLS = "{{ $polls }}";
     </script>
     @endauth
 
@@ -17,6 +16,31 @@
 
 {{-- <script defer src="{{ asset('/js/app.js') }}"></script> --}}
 <style src="{{ asset('/css/app.css') }}"></style>
+
+<script>
+    var decodeEntities = (function() {
+  // this prevents any overhead from creating the object each time
+  var element = document.createElement('div');
+
+  function decodeHTMLEntities (str) {
+    if(str && typeof str === 'string') {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = '';
+    }
+
+    return str;
+  }
+
+  return decodeHTMLEntities;
+})();
+    
+    var GPOLLS_ENCODED = "{{ $polls }}";
+    var GPOLLS = JSON.parse(decodeEntities(GPOLLS_ENCODED));
+</script>
 
 <div id="index"></div>
 
