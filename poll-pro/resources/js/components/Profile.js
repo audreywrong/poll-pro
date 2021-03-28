4f8b60823abc67925938d02b60756f9ed143ed98
Audrey <audreywrong@gmail.com>
2021-03-27 20:31:02 -0500

    moving profile around

--- poll-pro/resources/js/components/Profile.js
+++ poll-pro/resources/js/components/Profile.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/app.css";
 import WelcomeGraph from "./WelcomeGraph";
 
 const Profile = () => {
+    const [user] = useState(GUSER);
+
     return (
         <>
             <div className="top-profile-container">
+                <div className="welcome-message">
+                    <h1>Welcome {user}!</h1>
+                </div>
+                <div className="poll-message">
+                    <p>Poll Vote Distribution</p>
+                </div>
                 <div className="graph-container">
                     <WelcomeGraph />
-                </div>
-                <div className="welcome-message">
-                    <h1>Welcome 'user'!</h1>
                 </div>
             </div>
             <div className="bottom-profile-container">
