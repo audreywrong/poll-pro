a3958bc78284307af68611840f25db93f42d3326
Audrey <audreywrong@gmail.com>
2021-03-27 20:31:17 -0500

    placeholder changes and compiling

--- poll-pro/resources/views/dashboard/create.blade.php
+++ poll-pro/resources/views/dashboard/create.blade.php
@@ -29,14 +29,14 @@ Polls- Creation
                 <svg class="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                     <path
                         d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-                    </svg>
+                </svg>
             </li>
             <li class="flex items-center">
                 <a href="{{ route('poll.index') }}">Polls</a>
                 <svg class="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                     <path
                         d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-                    </svg>
+                </svg>
             </li>
             <li>
                 <a href="#" class="text-gray-500" aria-current="page">Create</a>
@@ -51,7 +51,7 @@ Polls- Creation
                     for="question">
                     Question
                 </label>
-                <input v-model="question" placeholder="Who is the best football player in the world?"
+                <input v-model="question" placeholder="Who is your favorite artist?"
                     class="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                     id="password" type="text">
             </div>
@@ -75,7 +75,7 @@ Polls- Creation
                 <div class="w-full flex items-center border-b border-b-2 border-teal-500 py-2">
                     <input @keyup.enter="addNewOption" v-model="newOption"
                         class="appearance-none bg-transparent border-none block w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                        type="text" placeholder="Luka Modric" aria-label="Full name">
+                        type="text" placeholder="Prince" aria-label="Full name">
                     <button @click.prevent="addNewOption"
                         class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
                         type="button">
@@ -166,8 +166,8 @@ Polls- Creation
                     newOption: '',
                     question: '',
                     options: [
-                        { value: '', placeholder: 'Cristiano Ronaldo'},
-                        { value: '', placeholder: 'Lionel Messi'},
+                        { value: '', placeholder: 'Freddie Mercury'},
+                        { value: '', placeholder: 'Dave Grohl'},
                     ],
                    error_message: '',
                }
