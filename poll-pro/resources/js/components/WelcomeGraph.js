4e0206ca9359268acf10b08567c155c2bcfad90e
Audrey <audreywrong@gmail.com>
2021-03-27 20:29:12 -0500

    edit graph
    to reflect data - needs actual data though

--- poll-pro/resources/js/components/WelcomeGraph.js
+++ poll-pro/resources/js/components/WelcomeGraph.js
@@ -1,20 +1,22 @@
+import { colors, withTheme } from "@material-ui/core";
 import * as React from "react";
 // import ReactDOM from "react-dom";
 import { RadialChart } from "react-vis";
 
 const WelcomeGraph = () => {
     const myData = [
-        { angle: 7, label: "Poll 1" },
-        { angle: 5, label: "Poll 2" },
-        { angle: 1, label: "Poll 3" },
+        { angle: 5, label: "Poll 3" },
+        { angle: 4, label: "Poll 5" },
+        { angle: 2, label: "Poll 6" },
     ];
 
     return (
         <>
-            <div style={{ height: 400, width: "100%" }}>
+            <div style={{ height: 300, width: "100%" }}>
                 <RadialChart
                     className="radial-chart"
                     showLabels={true}
+                    labelsRadiusMultiplier={0.9}
                     data={myData}
                     width={300}
                     height={300}
