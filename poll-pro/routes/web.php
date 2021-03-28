94b6d489dd584971ade83db19c51bb8bd45c2128
Audrey <audreywrong@gmail.com>
2021-03-27 19:42:05 -0500

    cleaning code

--- poll-pro/routes/web.php
+++ poll-pro/routes/web.php
@@ -21,5 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('/admin/polls/create', [Inani\Larapoll\Http\Controllers\PollManagerController::class, 'create'])->name('poll.create');
-// Route::get('/polls/details', [App\Http\Controllers\PollController::class, 'view_details'])->name('poll.details');
+Route::get('/polls/details', [App\Http\Controllers\PollController::class, 'view_details'])->name('poll.details');
